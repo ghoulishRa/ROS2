c3768951e6b80b15d9df19a88cbaddcfdc1e174e
Israel <a01027029@tec.mx>
2025-04-02 18:53:56 -0600

    it partially works

---
+++ launch_gazebo.sh
@@ -0,0 +1,4 @@
+
+#/bin/bash
+export TURTLEBOT3_MODEL=burger
+ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py x_pose:=-2.0 y_pose:=0.0
--- src/potMap/potMap/potMap_node.py
+++ src/potMap/potMap/potMap_node.py
@@ -36,8 +36,14 @@ class PotentialMap(Node):
         self.theta_rob = 0.0
 
         self.click = PointStamped()    #self.click_theta = 0.0
-        self.click_x = self.click.point.x
-        self.click_y = self.click.point.y
+
+        self.waypoints = [[0.0, 2.0], [2.0, 0.0], [0.0, -2.0], [-2.0, 0.0]]
+
+        self.click_x = self.waypoints[0][0]
+        self.click_y = self.waypoints[0][1]
+
+        self.d_goal = 0.0
+        self.counter = 0
 
         """Messages"""
         self.speed = Twist()
@@ -59,7 +65,7 @@ class PotentialMap(Node):
         self.k_att = 2.0 #attraction force to clickpoint
         self.k_rep = 1.0
 
-        self.k_linear = 0.01
+        self.k_linear = 0.03
         self.k_angular  = 1.0
 
 
@@ -105,7 +111,7 @@ class PotentialMap(Node):
 
         for i, deg in enumerate (self.angles):
             # if (0 <= deg <= (np.pi)/4) and (2*(np.pi)-(np.pi)/4 <= deg <= 2*(np.pi)):
-            if (self.ranges[i]<0.25):
+            if (self.ranges[i]<0.45):
                 self.Fx_rep += (1/self.ranges[i]) * np.cos(deg)
                 self.Fy_rep += (1/self.ranges[i]) * np.sin(deg)
 
@@ -118,75 +124,39 @@ class PotentialMap(Node):
             """Repulsion forces calculation"""
             #getting repulsion force magnitude and angle#
 
-            self.F_rep_ang = (np.arctan2(self.Fy_rep, self.Fx_rep)) - self.theta_rob  #add pi to invert direction
-            if self.F_rep_ang < 0.0:
-                self.F_rep_ang += 2.0*np.pi
-
-            if self.Fy_rep == 0 and self.Fx_rep == 0 :
-                self.F_rep_ang = 0
-
-            self.F_rep_mag = np.linalg.norm((self.Fx_rep, self.Fy_rep))  
-
-            print("Fx_rep: ", self.Fx_rep)
-            print("Fy_rep: ", self.Fy_rep)
-            print("theta_rep:",self.F_rep_ang)
-
-            # self.speed.linear.x = self.k_linear * self.F_rep_mag
-            # self.speed.angular.z = self.k_angular * self.F_rep_ang
-
-            #print("linear_speed:", self.speed.linear.x)
-            #print("angular_speed:", self.speed.angular.z)
-
-            #self.robot_speed.publish(self.speed)
-
             dx = self.pose_x - self.click_x
             dy = self.pose_y - self.click_y
 
             Fx_att = self.k_att * dx
             Fy_att = self.k_att * dy
 
-            print("Fx_att: ", Fx_att)
-            print("Fy_att: ", Fy_att)
-
-            print("yaw:", self.theta_rob)
-
-            d_goal = np.sqrt(dx**2 + dy**2)
-
-            F_att_ang  = np.arctan2(Fy_att,Fx_att)
-            F_att_mag = np.linalg.norm((Fx_att,Fy_att))
+            self.d_goal = np.sqrt(dx**2 + dy**2)
 
             Fx_total = Fx_att + (self.k_rep*self.Fx_rep)
             Fy_total = Fy_att + (self.k_rep*self.Fy_rep)
 
             F_total_ang = (np.arctan2(Fy_total,Fx_total) + np.pi) - self.theta_rob
+            F_total_ang = np.arctan2(np.sin(F_total_ang), np.cos(F_total_ang))
             F_total_mag = np.sqrt(Fx_total**2 + Fy_total**2)
-
-            print("F_total_ang", F_total_ang )
-            print("F_total_mag", F_total_mag )
 
             self.speed.linear.x = self.k_linear * F_total_mag
             self.speed.angular.z = (self.k_angular * F_total_ang)
 
-            if d_goal < 0.1:
-                self.speed.linear.x = 0.0
-                self.speed.angular.z = 0.0
+            self.state_machine()
             
-            self.rob_pose.pose.position.x = Fx_total
-            self.rob_pose.pose.position.y = Fy_total
-
-            ang_quat = transforms3d.euler.euler2quat(0.0, 0.0, F_total_ang)
-            self.rob_pose.pose.orientation.w = ang_quat[0]
-            self.rob_pose.pose.orientation.x = ang_quat[1]
-            self.rob_pose.pose.orientation.y = ang_quat[2]
-            self.rob_pose.pose.orientation.z = ang_quat[3]
-
-            # print ("click_x:",self.click_x)
-            # print ("click_y:",self.click_y)
-            # print("linear_speed:", self.speed.linear.x)
-            # print("angular_speed:", self.speed.angular.z)
-
             self.robot_speed.publish(self.speed)
-            self.vector.publish(self.rob_pose)
+            
+    
+    def state_machine (self):
+        if self.d_goal < 0.1:
+            print("entre a state machine")
+            self.counter += 1
+            self.click_x = self.waypoints[self.counter][0]
+            self.click_y = self.waypoints[self.counter][1]
+            if self.counter == len(self.waypoints)-1 :
+                self.speed.angular.z = 0.0
+                self.speed.linear.x = 0.0
+                self.counter = 0
 
 
 def main(args=None):
